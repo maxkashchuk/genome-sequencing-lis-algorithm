195e8cc566890cbb275ae82841a13930e26fc8ab
Maksym Kashchuk <39593404+maxkashchuk@users.noreply.github.com>
2026-05-02 13:27:15 +0200

    Provide example of sequence retrieval through the bioparser API, provide launch instructions in README.md

--- include/main.h
+++ include/main.h
@@ -9,7 +9,6 @@ namespace Main {
     
     int opt;
     
-    // Variables to store the values decoded by the parser
     int match_score = 0;
     int mismatch_penalty = 0;
     int gap_penalty = 0;
@@ -17,11 +16,20 @@ namespace Main {
 
     struct Sequence {
         public:
+            std::string name;
+            std::string data;
+
             Sequence(
-                const char*, std::uint32_t,
-                const char*, std::uint32_t) {
-            }
+                const char* name_ptr, std::uint32_t name_len,
+                const char* data_ptr, std::uint32_t data_len) : name(name_ptr, name_len),
+                                                                data(data_ptr, data_len) {
+                }
     };
+
+    // Srquence to binary template function (to be implemented)
+    void sequence_to_binary(std::vector<std::unique_ptr<Sequence>>& seq) {
+        return;
+    }
 }
 
 #endif
